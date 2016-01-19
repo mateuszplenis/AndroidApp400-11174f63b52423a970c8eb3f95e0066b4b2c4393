a266fb6614544cfdae0bac7599adc054e0420ba3
Michal <michal6wozniak@gmail.com>
2016-01-19 11:03:56 +0100

    Initial commit MP400

---
+++ app/src/main/java/pl/edu/ug/aib/firstApp/data/Person.java
@@ -0,0 +1,13 @@
+package pl.edu.ug.aib.firstApp.data;
+
+public class Person {
+
+    public String name;
+    public String company;
+
+    public Person(String name, String company) {
+        this.name = name;
+        this.company = company;
+    }
+
+}
